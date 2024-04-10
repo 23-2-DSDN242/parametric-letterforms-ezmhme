c7b1787ef5205cdd5763a0ade5804da5201029da
ezmhme <emzhemingway@gmail.com>
2024-04-10 20:02:06 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,9 +30,9 @@ const letterC = {
   "offsety": 0
 }
 
-const backgroundColor  = "#acf2e7";
+const backgroundColor  = "#26547c";
 
-const darkGreen  = "#26b29d";
+const yellow  = "#ffd166";
 const lightGreen  = "#30dfc4";
 const strokeColor  = "#0a2d27";
 
@@ -55,12 +55,12 @@ function draw () {
 
   // compute the center of the canvas
   let center_x = canvasWidth / 2;
-  let center_y = canvasHeight / 1.6;
+  let center_y = canvasHeight/2-100;
 
   // draw the letters A, B, C from saved data
-  drawLetter(center_x - 250, center_y, letterA);
-  drawLetter(center_x      , center_y, letterB);
-  drawLetter(center_x + 250, center_y, letterC);
+  drawLetter(center_x-400, center_y, letterA);
+  drawLetter(center_x-100, center_y, letterB);
+  drawLetter(center_x+200, center_y, letterC);
 }
 
 function drawLetter(posx, posy, letterData) {
@@ -70,10 +70,11 @@ function drawLetter(posx, posy, letterData) {
   let pos2y = posy + letterData["offsety"];
 
   // draw two circles
-  fill(darkGreen);
-  ellipse(posx, posy, 150, 150);
-  fill(lightGreen);
-  ellipse(pos2x, pos2y, size2, size2);
+  fill(yellow);
+  noStroke();
+  square(posx, posy, 200);
+  //fill(lightGreen);
+  //square(pos2x, pos2y, size2);
 }
 
 function keyTyped() {
