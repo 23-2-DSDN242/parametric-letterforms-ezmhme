c0ceb368a6314d695b8a211562f9b224cde47f7c
ezmhme <emzhemingway@gmail.com>
2024-04-16 21:42:02 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,8 +14,8 @@ const canvasHeight = 500;
 
 const letterA = {
   "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "offsetx": 300,
+  "offsety": 0
 }
 
 const letterB = {
@@ -30,11 +30,12 @@ const letterC = {
   "offsety": 0
 }
 
-const backgroundColor  = "#26547c";
+const backgroundColor  = "#f5e6e8";
 
-const yellow  = "#ffd166";
+const yellow  = "#aaa1c8";
 const lightGreen  = "#30dfc4";
 const strokeColor  = "#0a2d27";
+const thistle = "#d5c6e0";
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -55,12 +56,12 @@ function draw () {
 
   // compute the center of the canvas
   let center_x = canvasWidth / 2;
-  let center_y = canvasHeight/2-100;
+  let center_y = canvasHeight/ 2;
 
   // draw the letters A, B, C from saved data
-  drawLetter(center_x-400, center_y, letterA);
-  drawLetter(center_x-100, center_y, letterB);
-  drawLetter(center_x+200, center_y, letterC);
+  drawLetter(center_x/5, center_y-100, letterA);
+  // drawLetter(center_x, center_y, letterB);
+  // drawLetter(center_x, center_y, letterC);
 }
 
 function drawLetter(posx, posy, letterData) {
@@ -72,7 +73,10 @@ function drawLetter(posx, posy, letterData) {
   // draw two circles
   fill(yellow);
   noStroke();
-  square(posx, posy, 200);
+  triangle(posx+10, posy+200, pos2x-10, pos2y+200, 245, 130);
+  fill(thistle);
+  triangle(posx+80, posy+200, pos2x-80, pos2y+200, 245, 240);
+  // square(posx, posy, 200);
   //fill(lightGreen);
   //square(pos2x, pos2y, size2);
 }
